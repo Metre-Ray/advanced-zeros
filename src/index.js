31541f692779c7ddfa6a3fac2ab5aa8cface8666
Metre-Ray <dgary@tut.by>
2018-09-26 18:32:21 +0300

    more es6

--- src/index.js
+++ src/index.js
@@ -1,24 +1,28 @@
 module.exports = function getZerosCount(number, base) {
-  var multiplier = 0,
-      c = 1,
+  let multiplier = 0,
+      count = 1,
       counts = {},
-      temp = 0;
-  var mult = get_simple_multipliers(base);
-  if (mult.length === 1) {
+      temp = 0,
+      mult = 0,
+      length = 0,
+      answer = 0;
+  mult = get_simple_multipliers(base);
+  length = mult.length;
+  if (length === 1) {
     multiplier = mult[0];
   }
   else {
-    multiplier = mult[mult.length - 1];
-    for (let i = 0; i < mult.length - 1; i++) {
-      if (counts[mult[i]] === undefined) {counts[mult[i]] = 1;}
-      else {counts[mult[i]] += 1;}
-      if (mult[i] === multiplier) {c += 1;}
+    multiplier = mult[length - 1];
+    for (let i = 0; i < length - 1; i++) {
+      if (counts[mult[i]] === undefined) counts[mult[i]] = 1;
+      else counts[mult[i]] += 1;
+      if (mult[i] === multiplier) count += 1;
     }
   }
-  answer = Math.floor(count_number_of_occurrences(multiplier, number) / c);
+  answer = Math.floor(count_number_of_occurrences(multiplier, number) / count);
   for (el in counts) {
     temp = count_number_of_occurrences(parseInt(el), number);
-    if (Math.floor(temp / counts[el]) < answer) {answer = Math.floor(temp / counts[el]);}
+    if (Math.floor(temp / counts[el]) < answer) answer = Math.floor(temp / counts[el]);
   }
   return answer;
 }
@@ -34,8 +38,8 @@ function count_number_of_occurrences(mult, number) {
 }
 
 function get_simple_multipliers(n) {
-  if (is_prime(n)) {return [n];}
-  var multipliers = [],
+  if (is_prime(n)) return [n];
+  let multipliers = [],
       counts = [],
       i = 2;
   while (n > 1) {
@@ -43,14 +47,14 @@ function get_simple_multipliers(n) {
       multipliers.push(i);
       n = Math.floor(n / i);
     }
-    if (i === 2) {i += 1;}
-    else {i += 2;}
+    if (i === 2) i += 1;
+    else i += 2;
   }
   return multipliers;
 }
 
 function is_prime(n) {
-  if (n % 2 === 0) {return false;}
+  if (n % 2 === 0) return false;
   for (let i = 3; i < (n ** (1/2)) + 1; i += 2) {
     if (n % i === 0) {
       return false;
